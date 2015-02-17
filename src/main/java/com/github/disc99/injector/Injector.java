715032ab44cd2aaaf0040fe767d5649cde79438f
d.kanehira <disc99.o@gmail.com>
2015-02-18 01:30:46 +0900

    update

--- src/main/java/com/github/disc99/injector/Injector.java
+++ src/main/java/com/github/disc99/injector/Injector.java
@@ -1,10 +1,32 @@
 package com.github.disc99.injector;
 
+import java.util.List;
+
 public class Injector {
 
-    public <T> T getInstance(Class<T> class1) {
-        // TODO Auto-generated method stub
-        return null;
+    public <T> T getInstance(Class<T> clazz) {
+        try {
+            T instance = clazz.newInstance();
+
+            // inject fields
+            injectFields(instance);
+
+            // return instance
+            return instance;
+        } catch (InstantiationException | IllegalAccessException e) {
+            throw new InjectException("fail new instance.", e);
+        }
     }
 
+    private <T> void injectFields(T t) {
+        // TODO
+        List<?> classes = searchInjectClasses(t);
+
+    }
+
+    private <T> List<?> searchInjectClasses(T t) {
+        // TODO search @Inject fields
+
+        return null;
+    }
 }
